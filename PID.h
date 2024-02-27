57b5cfd04ed5cda089df9bd8ebf7c78211842a66
EricSteine-r <144844548+EricSteine-r@users.noreply.github.com>
2024-02-27 16:12:06 -0500

    Update PID.h

--- PID.h
+++ PID.h
@@ -1,24 +1,12 @@
 #ifndef _PID_H_
 #define _PID_H_
 
-class PIDImpl;
-class PID
-{
-    public:
-        // Kp -  proportional gain
-        // Ki -  Integral gain
-        // Kd -  derivative gain
-        // dt -  loop interval time
-        // max - maximum value of manipulated variable
-        // min - minimum value of manipulated variable
-        PID( double dt, double max, double min, double Kp, double Kd, double Ki );
+typedef struct PIDImpl PIDImpl;
+typedef struct PID PID;
 
-        // Returns the manipulated variable given a setpoint and current process value
-        double calculate( double setpoint, double pv );
-        ~PID();
-
-    private:
-        PIDImpl *pimpl;
-};
+PID *PID_create(double dt, double max, double min, double Kp, double Kd, double Ki);
+void PID_destroy(PID *pid);
+double PID_calculate(PID *pid, double setpoint, double pv);
 
 #endif
+
