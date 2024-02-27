b8d218efb8230c6c1eb652b40c908c2c5afb52c8
EricSteine-r <144844548+EricSteine-r@users.noreply.github.com>
2024-02-27 16:02:27 -0500

    Update PID.h

--- PID.h
+++ PID.h
@@ -1,60 +1,24 @@
-* This was taken from a very similar reaction wheel system
+#ifndef _PID_H_
+#define _PID_H_
 
-/**
- * This file is an ultra light/simple PID controller
- * library for Arduino.
- * 
- * Charles GRASSIN, 2019
- * MIT License
- */
+class PIDImpl;
+class PID
+{
+    public:
+        // Kp -  proportional gain
+        // Ki -  Integral gain
+        // Kd -  derivative gain
+        // dt -  loop interval time
+        // max - maximum value of manipulated variable
+        // min - minimum value of manipulated variable
+        PID( double dt, double max, double min, double Kp, double Kd, double Ki );
 
-#ifndef PID_CONTROLLER_H
-#define PID_CONTROLLER_H
-/* A standard PID controller class */
-class PIDController {
-private:
-  double kp, ki, kd, lastErr, cumulErr;
+        // Returns the manipulated variable given a setpoint and current process value
+        double calculate( double setpoint, double pv );
+        ~PID();
 
-protected:
-  virtual double calcError(double setPoint, double current){
-    return setPoint - current;
-  }
-  
-public:
-  PIDController(double kp, double ki, double kd) : kp(kp), ki(ki), kd(kd), lastErr(0), cumulErr(0){}
-  
-  double compute(double setPoint,double current, long detaT){
-     /*Compute all the working error variables*/
-     double error = calcError(setPoint, current);
-     double dErr = (detaT==0)?0:calcError(error, lastErr) / detaT;
-     
-     /*Remember some variables for next time*/
-     lastErr = error;
-     cumulErr += error * detaT;
-
-     /*Compute PID Output*/
-     return kp * error + ki * cumulErr +  kd * dErr;
-  }
-  
-  void reset(){
-    lastErr=0;cumulErr=0;
-  }
+    private:
+        PIDImpl *pimpl;
 };
 
-/* A PID controller to control angles*/
-class PIDAngleController: public PIDController{
-  
-public:
-  PIDAngleController(double kp, double ki, double kd) : PIDController(kp, ki, kd) {}
-
-protected:
-  virtual double calcError(double setPoint, double current){
-    double distance = fmod(setPoint - current , 360.0);
-    if (distance < -180)
-        return distance + 360;
-    else if (distance > 179)
-        return distance - 360;
-    return distance;
-  }
-};
 #endif
