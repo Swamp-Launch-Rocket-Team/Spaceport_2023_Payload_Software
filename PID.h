0e8cffbe7d80e5562cec9331e7d4d77ea785deeb
EricSteine-r <144844548+EricSteine-r@users.noreply.github.com>
2024-03-05 11:53:55 -0500

    Update PID.h

--- PID.h
+++ PID.h
@@ -1,12 +1,1 @@
-#ifndef _PID_H_
-#define _PID_H_
-
-typedef struct PIDImpl PIDImpl;
-typedef struct PID PID;
-
-PID *PID_create(double dt, double max, double min, double Kp, double Kd, double Ki);
-void PID_destroy(PID *pid);
-double PID_calculate(PID *pid, double setpoint, double pv);
-
-#endif
 
